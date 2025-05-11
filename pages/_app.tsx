2f8833f8161915b11ace4b4f97f37a763db6a1f3
Danil <danil.lutsiv@geniussports.com>
2025-05-12 01:34:35 +0300

    Add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 // import { SessionProvider } from 'next-auth/react';
 // import Layout from '@/components/layout';
 import "../styles/globals.css";
+import "../styles/theme.css";
 
 export default function MyApp({
                                   Component,
